21a798d3f51bcee2643ca3fc45b47e22dd00d938
Mahin-Hasan <mahinmtrs@gmail.com>
2024-06-19 21:52:47 +0600

    explored basic functions, functions with objects, never type

--- TypeScript Basics/dist/index.js
+++ TypeScript Basics/dist/index.js
@@ -52,3 +52,80 @@ const obj4 = {
 };
 obj4.func(4, 5);
 console.log(obj, obj2, obj3, obj4);
+// -------------------------------------
+//functions
+//1
+// type FuncType1 = (n: number, m: number, l?: number) => number 
+// //optinal parameter
+// const func1: FuncType1 = (n, m, l) => {
+//     if (typeof l === 'undefined') return n*m;
+//     return n * m * l;
+// }
+// const newRes = func1(43, 56)
+// console.log(newRes);
+//2 //default parameter
+// type FuncType1 = (n: number, m: number, l?: number) => number //l needed be made optional or else it will give error
+// const func1: FuncType1 = (n, m, l = 22) => {
+//     return n * m * l;
+// }
+// const newRes = func1(43, 56)
+// console.log(newRes);
+//3 rest operator
+// type FuncType1 = (...m: number[]) => number[];
+// const func1: FuncType1 = (...m) => {
+//     return m;
+// }
+// func1(12, 45, 234, 657, 98);
+//4 functions with objects
+//one way
+// const getData = (product: {
+//     name: string,
+//     stock: number,
+//     price: number,
+//     photo: string,
+// }): void => {
+//     console.log(product);
+// }
+//cleaner way
+// type GetDataType = (product: {
+//     name: string,
+//     stock: number,
+//     price: number,
+//     photo: string,
+// }) => void;
+// const getData: GetDataType = (product) => {
+//     console.log('Passed product', product);
+// }
+// const productOne = {
+//     name: "Razer",
+//     stock: 12,
+//     price: 35556,
+//     photo: "give a photo url"
+// }
+// getData(productOne)
+//more structured way
+// interface Product {
+//     name: string,
+//     stock: number,
+//     price: number,
+//     photo: string,
+//     readonly id: string,
+// }
+// type GetDataType = (product: Product) => void;
+// const getData: GetDataType = (product) => {
+//     product.name="Asus"
+//     // product.id ="sosamd" cannot assign becaus it is in read only mode
+//     console.log('Passed product', product);
+// }
+// const productOne: Product = {
+//     name: "Razer",
+//     stock: 12,
+//     price: 35556,
+//     photo: "give a photo url",
+//     id: "something"
+// }
+// getData(productOne)
+// Never Type
+const errorhandler = () => {
+    throw new Error();
+};
