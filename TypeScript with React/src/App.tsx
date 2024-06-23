e74a0446ceac4e39e01ae9cce67c8bb9812868e4
Mahin-Hasan <mahinmtrs@gmail.com>
2024-06-23 18:12:03 +0600

    explored useReducer hook

--- TypeScript with React/src/App.tsx
+++ TypeScript with React/src/App.tsx
@@ -1,46 +1,108 @@
-import { ReactNode, createContext, useState } from "react";
-import Box2 from "./components/Box2";
 
+//Reducer in TypeScript
 
-type ThemeType = "light" | "dark";
+import { useReducer } from "react"
 
-interface ThemeContextType {
-  theme: ThemeType;
-  toggleTheme: () => void;
+type StateType = {
+  count: number
 }
-export const ThemeContext = createContext<ThemeContextType>({
-  theme: "light",
-  toggleTheme: () => { }
-})
-//need to make a component or else the context will not re render
-const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useState<ThemeType>("light")
 
-  const toggleTheme = () => {
-    setTheme((prev) => (prev === "light" ? "dark" : "light"))
+type ActionType =
+  | { type: "Increment", payload: number }
+  | { type: "Deccrement", payload: number }
+
+
+const reducer = (state: StateType, action: ActionType): StateType => {
+  switch (action.type) {
+    case "Increment":
+      return { count: state.count + action.payload }
+    case "Deccrement":
+      return { count: state.count - action.payload }
+    default:
+      return state;
   }
+}
 
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  )
+const initialState: StateType = {
+  count: 0
 }
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const increment = (): void => {
+    dispatch({
+      type: "Increment",
+      payload: 1,
+    })
+  }
+  const decrement = (): void => {
+    dispatch({
+      type: "Deccrement",
+      payload: 1,
+    })
+  }
+
   return (
     <>
       <div>
-        <ThemeProvider>
-          <div>Test</div>
-          <Box2/>
-        </ThemeProvider>
+        <h1>check</h1>
+        <p>Count: {state.count}</p>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
       </div>
     </>
   )
 }
 
 export default App
+
+
+
+//explored context by using theme
+// import { ReactNode, createContext, useState } from "react";
+// import Box2 from "./components/Box2";
+
+
+// type ThemeType = "light" | "dark";
+
+// interface ThemeContextType {
+//   theme: ThemeType;
+//   toggleTheme: () => void;
+// }
+// export const ThemeContext = createContext<ThemeContextType>({
+//   theme: "light",
+//   toggleTheme: () => { }
+// })
+// //need to make a component or else the context will not re render
+// const ThemeProvider = ({ children }: { children: ReactNode }) => {
+//   const [theme, setTheme] = useState<ThemeType>("light")
+
+//   const toggleTheme = () => {
+//     setTheme((prev) => (prev === "light" ? "dark" : "light"))
+//   }
+
+//   return (
+//     <ThemeContext.Provider value={{ theme, toggleTheme }}>
+//       {children}
+//     </ThemeContext.Provider>
+//   )
+// }
+
+// function App() {
+//   return (
+//     <>
+//       <div>
+//         <ThemeProvider>
+//           <div>Test</div>
+//           <Box2/>
+//         </ThemeProvider>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default App
 
 
 
--- TypeScript with React/src/components/Box2.tsx
+++ TypeScript with React/src/components/Box2.tsx
@@ -6,9 +6,14 @@ import { ThemeContext } from "../App";
 const Box2 = () => {
     const { theme, toggleTheme } = useContext(ThemeContext)
 
-    alert(theme)
+    // alert(theme)
     return (
-        <div className="boxContainer">
+        <div className="boxContainer"
+        style={{
+            backgroundColor: theme === "dark" ? "rgb(40,40,40)" : "white",
+            color: theme === "dark" ? "white" : "rgb(40,40,40)",
+        }}
+        >
             <h1>Box 1</h1>
             <button onClick={toggleTheme}>Change Theme</button>
         </div>
