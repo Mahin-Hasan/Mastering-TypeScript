b14df92eda8c8fba0b2d15174adfa3d7afad7eff
Mahin-Hasan <mahinmtrs@gmail.com>
2024-06-27 04:50:41 +0600

    Created basic project template with necessary librarires also MUI setup

---
+++ TypeScript Language Learning App/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Route, Router, Routes } from "react-router-dom"
+import Header from "./components/Header"
+
+const App = () => {
+
+  return (
+    <Router>
+      <Header/>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
