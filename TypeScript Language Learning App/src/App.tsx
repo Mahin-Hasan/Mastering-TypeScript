05b087a045de1f8905b21bbeee70b4bac2dd3fed
Mahin-Hasan <mahinmtrs@gmail.com>
2024-06-27 05:19:38 +0600

    designed navbar

--- TypeScript Language Learning App/src/App.tsx
+++ TypeScript Language Learning App/src/App.tsx
@@ -1,14 +1,26 @@
-import { BrowserRouter as Route, Router, Routes } from "react-router-dom"
-import Header from "./components/Header"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { Suspense, lazy } from "react"
+import Loader from "./components/Loader"
+//importing Home using code splitting instead of directly importing
+const Home = lazy(() => import("./components/Home"))
+const Learning = lazy(() => import("./components/Learning"))
+const Header = lazy(() => import("./components/Header"))
+const Quiz = lazy(() => import("./components/Quiz"))
+const Result = lazy(() => import("./components/Result"))
 
 const App = () => {
-
+//5h 35m
   return (
     <Router>
-      <Header/>
-      <Routes>
-        <Route />
-      </Routes>
+      <Header />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/learning" element={<Learning />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </Suspense>
     </Router>
   )
 }
--- TypeScript Language Learning App/src/components/Header.tsx
+++ TypeScript Language Learning App/src/components/Header.tsx
@@ -1,9 +1,22 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+
+const styles ={
+    color:"white",
+    margin:"0.5rem",
+    textDecoration:"none"
+}
+
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h5" mr={"auto"} textTransform={"uppercase"}>Lang Learn!!!</Typography>
+                <Link style={styles} to={"/"}>Home</Link>
+            </Toolbar>
+        </AppBar>
     );
 };
 
